509d4570ed808327bf37fa832524da111b8179ad
Osmar <juninhomachado579@gmail.com>
2024-06-11 09:56:11 -0500

    dev: random number, nummaxtry changes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-let aleatorio = Math.floor(Math.random() * 50 + 1);
+let aleatorio = Math.floor(Math.random() * 100 + 101);
 let num 
-let numMaxTry = 5;
+let numMaxTry = 10;
 
-console.log("Vamos brincar de adivinhar números?");
+console.log("Vamos brincar de adivinhar números?\nTente adivinhar um número de 100 a 200");
 console.log("Você tem " + numMaxTry + " tentativas! Boa sorte!");
 
 process.stdin.on("data", function(data){
@@ -21,8 +21,8 @@ process.stdin.on("data", function(data){
         if(numMaxTry == 0){
             console.log("Suas tentativas acabaram! " + aleatorio + " era o número secreto");
             console.log("Tente novamente!");
-            numMaxTry = 5;
-            aleatorio = Math.floor(Math.random() * 50 + 1);
+            numMaxTry = 10;
+            aleatorio = Math.floor(Math.random() * 100 + 101);
         }
     }
 });
